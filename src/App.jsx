178607c168b55087d92455356cafb1df841428b4
wacho91 <criskol.71@gmail.com>
2024-11-14 11:19:49 -0500

    Navba Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./component/Navbar/Navbar";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="overflow-hidden">
+      <Navbar />
+    </div>
   )
 }
