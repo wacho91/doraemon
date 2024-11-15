3f6d4bfaf6a5d89c2cb9983dbccdc63e03e5e4f3
wacho91 <criskol.71@gmail.com>
2024-11-15 09:52:37 -0500

    Nabvar Component arquitecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Navbar from "./component/Navbar/Navbar";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+      easing: "ease-in-sine",
+      offset: 100,
+    });
+  })
+
   return (
     <div className="overflow-hidden">
       <Navbar />
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,70 @@
+import { FaSearch } from "react-icons/fa";
+import { GiAtomicSlashes } from "react-icons/gi";
+import { MdArrowDropDown } from "react-icons/md";
 
+
+const MenuLinks = [
+  {
+    name: "Episodes",
+    link: "#/",
+  },
+  {
+    name: "Store",
+    link: "#/store",
+  },
+  {
+    name: "Toys",
+    link: "#/toys",
+  },
+  {
+    name: "3D videos",
+    link: "#/videos",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>
-        Navbar Component
-    </div>
+    <nav>
+      <div>
+        <div>
+          {/* logo section */}
+          <div>
+            <GiAtomicSlashes />
+              <div>
+                  <span>Dora</span>
+                  <span>emon</span>
+              </div>
+          </div>
+          {/* links section */}
+          <div>
+            <ul>
+              {MenuLinks.map((data, index) => {
+                return (
+                  <li key={index}>
+                    <a href={data.link}>
+                      {data.name}
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          {/* Search section */}
+            <div>
+              <input type="text" />
+              <div>
+                <FaSearch />
+              </div>
+              <div>
+                Eng
+                <span>
+                  <MdArrowDropDown />
+                </span>
+              </div>
+           </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
