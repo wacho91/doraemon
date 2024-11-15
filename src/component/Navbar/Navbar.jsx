53463ae58b60a265cb31a2c6e5afe124e381ce74
wacho91 <criskol.71@gmail.com>
2024-11-15 10:10:50 -0500

    Navbar component complete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -25,23 +25,23 @@ const MenuLinks = [
 const Navbar = () => {
   return (
     <nav>
-      <div>
-        <div>
+      <div className="container py-4">
+        <div className="flex justify-between items-center">
           {/* logo section */}
-          <div>
-            <GiAtomicSlashes />
-              <div>
-                  <span>Dora</span>
-                  <span>emon</span>
+          <div className="flex items-center gap-4">
+            <GiAtomicSlashes className="text-6xl text-primary" />
+              <div className="flex flex-col text-2xl font-bold text-gray-600 leading-5">
+                <span>Dora</span>
+                <span>emon</span>
               </div>
           </div>
           {/* links section */}
-          <div>
-            <ul>
+          <div className="hidden md:block">
+            <ul className="center">
               {MenuLinks.map((data, index) => {
                 return (
                   <li key={index}>
-                    <a href={data.link}>
+                    <a className="navlink" href={data.link}>
                       {data.name}
                     </a>
                   </li>
@@ -50,15 +50,19 @@ const Navbar = () => {
             </ul>
           </div>
           {/* Search section */}
-            <div>
-              <input type="text" />
-              <div>
-                <FaSearch />
+            <div className="flex items-center gap-6">
+              <input 
+                type="text" 
+                placeholder="Search" 
+                className="hidden sm:block py-2 px-4 rounded-full cursor-pointer" 
+              />
+              <div className="bg-primary hover:bg-primary/80 p-3 rounded-full cursor-pointer">
+                <FaSearch className="text-white text-sm" />
               </div>
-              <div>
+              <div className="center navlink cursor-pointer">
                 Eng
                 <span>
-                  <MdArrowDropDown />
+                  <MdArrowDropDown className="text-xl text-primary" />
                 </span>
               </div>
            </div>
